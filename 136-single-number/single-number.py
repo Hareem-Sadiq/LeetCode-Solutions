12db3935ec0d711bd9089dccaf405495b74ce003
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-05-09 23:20:35 +0500

    Time: 3132 ms (15.17%) | Memory: 13.9 MB (41.09%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,11 @@
-import numpy as np
 class Solution(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        num_arr = np.array(nums)
-        unique_elements, counts = np.unique(num_arr, return_counts=True)
-        return(int(unique_elements[counts == 1]))
+        for num in nums:
+            duplicate=nums.count(num)
+            if (duplicate==1):
+                return (num)
+            
