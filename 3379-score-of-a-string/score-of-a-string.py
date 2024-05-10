c62ded4ad2f52cc3179af6a929cea9c57728ac13
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-05-10 23:48:07 +0500

    Time: 14 ms (73.33%) | Memory: 11.5 MB (87.96%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -4,10 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        score = 0
-        prev_char = ord(s[0])  # Initialize with the ASCII value of the first character
-        for i in range(1, len(s)):
-            curr_char = ord(s[i])
-            score += abs(curr_char - prev_char)
-            prev_char = curr_char
-        return score
+        score=0
+        for i in range(1,len(s)):
+            score+=abs(ord(s[i-1])-ord(s[i]))
+        return(score)
