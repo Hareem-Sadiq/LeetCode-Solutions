c96f9bede5db537aa2567543d8573b26a9a018e5
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-05-13 23:39:20 +0500

    Time: 20 ms (32.75%) | Memory: 12 MB (31.57%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -5,7 +5,5 @@ class Solution(object):
         :rtype: str
         """
         words = s.strip().split()
-        # Reverse the order of words
         words.reverse()
-        # Join the words back together with a single space
         return ' '.join(words)
