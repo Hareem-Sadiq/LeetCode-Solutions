026ba3b34d56ea4165ebfece074b4a8faa5b094d
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-05-15 23:56:42 +0500

    Time: 15 ms (53.02%) | Memory: 11.8 MB (24.65%) - LeetSync

--- 520-detect-capital/detect-capital.py
+++ 520-detect-capital/detect-capital.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        len_char=len(word)
         count=0
         index=None
         for char in word:
@@ -13,7 +12,7 @@ class Solution(object):
                 if char==word[0]:
                     index=True
                 continue
-        if (count==0)or (count==len_char) or (index and count==1):
+        if (count==0)or (count==len(word)) or (index and count==1):
             return(True)
         else:
             return(False)
