84041e969e1a740b21dab64d2ad3e2981ea17d53
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-05-17 00:02:15 +0500

    Time: 7 ms (97.58%) | Memory: 11.6 MB (58.70%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isValid(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        v=0
+        consonant=0
+        vowels=['a','e','i','o','u'] 
+        if len(word)>=3 and word.isalnum():
+            for char in word.lower():
+                if char in vowels:
+                    v+=1
+                elif(ord(char)>=48 and ord(char)<=57):
+                    pass
+                elif (ord(char)>=97 and ord(char)<=122):
+                    consonant+=1
+        else:
+            return(False)
+
+        if (consonant>=1 and v>=1):
+            return (True)
+        else:
+            return (False)
+
+                
+                
+            
