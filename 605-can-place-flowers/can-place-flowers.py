35bd73d825c515ce8403c3fa6adff581ba96dbf6
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-06-01 01:54:15 +0500

    Time: 103 ms (98.96%) | Memory: 12.3 MB (57.75%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -6,14 +6,15 @@ class Solution(object):
         :rtype: bool
         """
         count = 0
-        i = 0
-        while i < len(flowerbed):
+        for i in range(len(flowerbed)):
             if flowerbed[i] == 0:
-                prev_empty = (i == 0) or (flowerbed[i - 1] == 0)
+                prev_empty = (i == 0) or (flowerbed[i-1]==0)
                 next_empty = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
                 if prev_empty and next_empty:
                     flowerbed[i] = 1
                     n -= 1
-            i += 1
 
-        return( n <= 0)
+        if n<=0:
+            return(True)
+        else:
+            return(False)
