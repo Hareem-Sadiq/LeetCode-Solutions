557a2da8e2bbfe25be87a8eefffd0db08d308398
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-06-06 11:34:06 +0500

    Time: 24 ms (5.27%) | Memory: 11.8 MB (62.05%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        st=''
+        j=0
+        for i in s:
+            while j< len(t):
+                if t[j]==i:
+                    st=st+t[j]
+                    j=j+1
+                    break
+                else:
+                    j=j+1
+        if s==st:
+            return(True)
+        else:
+            return(False)
