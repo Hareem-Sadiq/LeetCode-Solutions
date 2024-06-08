9c9c6f841a471267d00fe893a6146aec9fb415f1
Hareem Sadiq <131405204+Hareem-Sadiq@users.noreply.github.com>
2024-06-08 23:59:55 +0500

    Time: 108 ms (55.40%) | Memory: 13.1 MB (5.89%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -4,16 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        total_sum = sum(nums)
-        left_sum = 0
-
-        for i, num in enumerate(nums):
-            right_sum = total_sum - left_sum - num
-        
-            if left_sum == right_sum:
+        n = len(nums)
+        sumLeft = [0] * n
+        sumRight = [0] * n
+    
+        for i in range(1, n):
+            sumLeft[i] = sumLeft[i - 1] + nums[i - 1]
+    
+        for i in range(n - 2, -1, -1):
+            sumRight[i] = sumRight[i + 1] + nums[i + 1]
+    
+        for i in range(n):
+            if sumLeft[i] == sumRight[i]:
                 return i
-        
-            left_sum += num
 
-        return -1 
+        return -1
         
